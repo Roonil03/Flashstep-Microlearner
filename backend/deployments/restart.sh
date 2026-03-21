5ba123c0f40ba1a01f7b0bec6c9869b5c666ea93
Roonil03 <67649149+Roonil03@users.noreply.github.com>
2026-03-21 10:01:02 +0530

    everything is about docker not breaking

--- backend/deployments/restart.sh
+++ backend/deployments/restart.sh
@@ -4,4 +4,8 @@ docker system prune -a -f
 docker ps -a
 docker images -a
 docker volume ls
-docker compose up --build
+docker compose up --build -d
+- name: Create .env
+  run: |
+    echo "POSTGRES_PASSWORD=${{ secrets.DB_PASSWORD }}" >> .env
+    echo "JWT_SECRET=${{ secrets.JWT_SECRET }}" >> .env
