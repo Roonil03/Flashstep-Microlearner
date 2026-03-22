b82e1592ba7b08fb22d308814b615d3bdfe471be
Roonil03 <aryanvivek05@gmail.com>
2026-03-22 11:31:51 +0530

    added images

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
+import 'core/storage/app_database.dart';
+import 'core/storage/database_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
