60eb2c8028af327f0a88e9f7d012b437dcc3e482
Roonil03 <aryanvivek05@gmail.com>
2026-04-06 19:02:11 +0530

    api Configurations changed in frontend v2

--- frontend/lib/core/config/api_config.dart
+++ frontend/lib/core/config/api_config.dart
@@ -1,8 +1,7 @@
 class ApiConfig {
   static String host = 'flashstep-api.onrender.com';
-  static int port = 8080;
 
   static String apiPrefix = '/api/v1';
 
-  static String get baseUrl => 'https://$host:$port$apiPrefix';
+  static String get baseUrl => 'https://$host$apiPrefix';
 }
